2b3b38089b151c1a88059f26f7045f2b59d56bc4
Dylan Singhabahu <108521278+DylanSinghabahu@users.noreply.github.com>
2022-07-03 02:56:55 +1000

    Add files via upload

--- simpleGuess.java
+++ simpleGuess.java
@@ -9,14 +9,16 @@ public class simpleGuess {
     private static final String ANSI_YELLOW = "\u001B[33m";
     private static int errors = 1;
 
-
-
+    public static void intro() {
+        System. out. println(ANSI_GREEN + "Loading...." + ANSI_RESET);
+        System. out. println(ANSI_YELLOW + "A number has been selected from 1-10 (inclusive).\nGuess the number. You have 3 chances!" + ANSI_RESET);
+    }
 
     public static void question() {
-        System.out.println("================================");
-        System.out.println("Hello, welcome to my Guessing Game! \nPlease choose a game:");
-        System.out.println("1. Guess the number \n2. Hangman");
-        System.out.println("================================");
+        System.out.println("=========================================");
+        System.out.println("== Hello, welcome to my Guessing Game! == \n== Please choose a game:               ==");
+        System.out.println("== 1. Guess the number                 == \n== 2. Hangman                          ==");
+        System.out.println("=========================================");
     }
 
     public static double optionOne() {
@@ -37,33 +39,37 @@ public class simpleGuess {
                 }
                 while (confirmation == 'N') {
                     int key = (int)optionOne();
-                    System.out.println(key);
-                    System. out. println(ANSI_GREEN + "Loading...." + ANSI_RESET);
-                    System. out. println(ANSI_YELLOW + "A number has been selected from 1-10 (inclusive).\nGuess the number. you have 3 chances!" + ANSI_RESET);
+                    intro();
                     int guess = sc.nextInt();
-                    for (int i = 2; i > 0; i--) {
+                    int i = 2;
+                    for (i=i; i > 0; i--) {
                         if (guess == key) {
                             System.out.println("Nice, you guessed right.");
                             break;
                         }
-                        else {
-                            if (i > 1) {
-                                System.out.println("Try again, you have " + i + " guesses left");
-                                errors++;
-                                guess = sc.nextInt();
-                            }
-                            else {
-                                System.out.println("Try again, you have " + i + " guess left");
-                                errors++;
-                                guess = sc.nextInt();
-                            }
+                        else if (i > 1) {
+                            System.out.println("Try again, you have " + i + " guesses left");
+                            guess = sc.nextInt();
+                        }
+                        if (i == 1) {
+                            System.out.println("Try again, you have " + i + " guess left");
+                            guess = sc.nextInt();
+                        }
+                        if (guess == key) {
+                            System.out.println("Nice, you guessed right.");
+                            break;
                         }
                     }
-                    if (errors == 3) {
+                    if (i == 0) {
                         System.out.println("Unfortunately you've ran out of guesses. The answer was " + key);
                     }
                     System.out.println("Want to try again (Y) or return to other options (R)?");
                     char secondConfirmation = sc.next().toUpperCase().charAt(0);
+                    while (secondConfirmation != 'R' && secondConfirmation != 'Y') {
+                        System.out.println("Enter the letter or a word beginning with R to return to other options");
+                        System.out.println("Enter the letter or a word beginning with Y to play again");
+                        secondConfirmation = sc.next().toUpperCase().charAt(0);
+                    }
                     if (secondConfirmation == 'Y') {
                         confirmation = 'N';
                     }
